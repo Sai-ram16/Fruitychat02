2bac6abeec461509ef29a7d96a9e9d41272dc071
sai-ram16 <sairampaka2916@gmail.com>
2024-08-20 21:31:53 +0100

    fruits information activity completed

--- app/src/main/java/com/sairam/fruitychat/InformationViewActivity.kt
+++ app/src/main/java/com/sairam/fruitychat/InformationViewActivity.kt
@@ -66,8 +66,149 @@ fun fruitychat(name: String, modifier: Modifier = Modifier) {
                 .requiredHeight(height = 980.dp)
                 .background(color = Color(0xFFFFA500))
         ) {
-            ///1
+            Image(
+                painter = painterResource(id = R.drawable.mangoes),
+                contentDescription = "",
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 0.dp,
+                        y = 82.dp)
+                    .requiredWidth(width = 375.dp)
+                    .requiredHeight(height = 463.dp))
+            Image(
+                painter = painterResource(id = R.drawable.star),
+                contentDescription = "",
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 20.dp,
+                        y = 468.dp)
+                    .requiredWidth(width = 375.dp)
+                    .requiredHeight(height = 163.dp))
 
+
+
+
+
+
+
+
+            Text(
+                text = "£ 40.00",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 20.sp),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 281.dp,
+                        y = 567.dp))
+            Text(
+                text = "Mangoes" +
+                        "",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 27.dp,
+                        y = 567.dp))
+
+            Text(
+                text = "The mango is a tropical stone fruit and member of the drupe family. This is a type of plant food with a fleshy outer section that surrounds a shell or a pit. This pit contais a seed.",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Medium),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 27.dp,
+                        y = 641.dp))
+
+
+            Box(
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 27.dp,
+                        y = 748.dp)
+                    .requiredSize(size = 30.dp)
+                    .clip(shape = CircleShape)
+                    .background(color = Color(0xFFFE6500)))
+            Box(
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 107.dp,
+                        y = 748.dp)
+                    .requiredSize(size = 30.dp)
+                    .clip(shape = CircleShape)
+                    .background(color = Color(0x75D6FF22)))
+
+            Text(
+                text = "+",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Medium),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 38.dp,
+                        y = 754.dp))
+            Text(
+                text = "1",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Medium),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 80.dp,
+                        y = 751.dp))
+
+            Text(
+                text = "-",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Medium),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 119.dp,
+                        y = 754.dp))
+            val localContext = LocalContext.current
+            Box(
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 103.dp,
+                        y = 838.dp)
+                    .requiredWidth(width = 235.dp)
+                    .requiredHeight(height = 50.dp)
+                    .clip(shape = RoundedCornerShape(15.dp))
+                    .background(color = Color(0x774CAF50))
+                    .clickable {
+                        localContext.startActivity(
+                            Intent(localContext, AddtoCartActivity::class.java)
+                        )
+                    }
+            )
+            Text(
+                text = "Add to Cart",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 181.dp,
+                        y = 853.dp))
+            Text(
+                text = "No.of kgs to buy",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 18.dp,
+                        y = 707.dp))
         }
     }
 }
